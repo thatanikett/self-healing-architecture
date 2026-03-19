e9d62d08637bc0a4adf2601a4277baebe40113b4
thatanikett <aniketsingh4269@gmail.com>
2026-03-20 01:18:10 +0530

    feat: MultiAZ availiblity - min_size & desired_capacity increase to atleast 2 instances -  both deployed on different subnets

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "app_asg" {
   name                      = "${var.project_name}-asg"
-  min_size                  = 1
-  max_size                  = 3
-  desired_capacity          = 1
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
   vpc_zone_identifier       = data.aws_subnets.default.ids
   health_check_type         = "ELB"
   health_check_grace_period = 300
